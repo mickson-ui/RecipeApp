a17cb0190b856f496dddcb549b6a69b2bdd420eb
Mickson Bonsu Bossman <mbonsu69@outlook.com>
2024-03-16 18:44:27 +0100

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Mickson Bossman on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
